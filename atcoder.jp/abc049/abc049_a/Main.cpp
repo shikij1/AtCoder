132a960749f3198c72680d7d3dd7733381dc48b1
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-16 19:58:40 +0900

    [AC] abc049 abc049_a

---
+++ atcoder.jp/abc049/abc049_a/Main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+  char c;
+  cin >> c;
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+    cout << "vowel" << endl;
+  }
+  else{
+    cout << "consonant" << endl;
+  }
+}
