fd454c85c9fe00c883c5a95186cc5b7b6e1eeca2
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-17 13:35:03 +0900

    [AC] APG4b APG4b_cg

---
+++ atcoder.jp/APG4b/APG4b_cg/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+  string answer = "NO";
+  for (int i = 0; i < 4; i++){
+    if (data.at(i) == data.at(i + 1)){
+      answer = "YES";
+      break;
+    }
+    
+  }
+  cout << answer << endl;
+}
