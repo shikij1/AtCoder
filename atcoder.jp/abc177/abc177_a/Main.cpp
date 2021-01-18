9ad33aac410c11a875c898c5aafffeaebf0ca24d
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-18 13:27:38 +0900

    [AC] abc177 abc177_a

---
+++ atcoder.jp/abc177/abc177_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float d, t, s;
+    cin >> d >> t >> s;
+    if (d / s <= t)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
