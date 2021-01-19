1da7e88467147976baa8f89c928465138221a008
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-19 16:24:45 +0900

    [AC] abc174 abc174_a

---
+++ atcoder.jp/abc174/abc174_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X;
+    cin >> X;
+    if (X >= 30)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
