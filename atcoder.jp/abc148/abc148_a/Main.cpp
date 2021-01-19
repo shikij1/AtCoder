fcf625f421c52a342789c09f33e24faea40098f5
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-20 02:50:05 +0900

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a + b == 3)
+        cout << 3 << endl;
+    else if (a + b == 4)
+        cout << 2 << endl;
+    else
+        cout << 1 << endl;
+}
