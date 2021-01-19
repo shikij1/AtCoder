ce0ff7a3874dccc15abb12be5bbe36723721a575
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-19 16:49:10 +0900

    [AC] abc171 abc171_a

---
+++ atcoder.jp/abc171/abc171_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s;
+    cin >> s;
+    if (isupper(s))
+        cout << 'A' << endl;
+    else
+        cout << 'a' << endl;
+}
