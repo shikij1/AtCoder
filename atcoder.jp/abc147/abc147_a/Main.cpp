0ef7fac0ca319bf9b95866f06e09ade64e07197b
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-20 03:28:24 +0900

    [AC] abc147 abc147_a

---
+++ atcoder.jp/abc147/abc147_a/Main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a1, a2, a3;
+    cin >> a1 >> a2 >> a3;
+    a1 + a2 + a3 >= 22 ? cout << "bust" << endl : cout << "win" << endl;
+}
