6b08feddd7e67e2092650d53e78b826ff4fd4731
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-19 21:26:06 +0900

    [AC] abc068 abc068_b

---
+++ atcoder.jp/abc068/abc068_b/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main()
+{
+    cin >> N;
+    if (N >= 64)
+    {
+        cout << 64 << endl;
+    }
+    else if (N >= 32)
+    {
+        cout << 32 << endl;
+    }
+    else if (N >= 16)
+    {
+        cout << 16 << endl;
+    }
+    else if (N >= 8)
+    {
+        cout << 8 << endl;
+    }
+    else if (N >= 4)
+    {
+        cout << 4 << endl;
+    }
+    else if (N >= 2)
+    {
+        cout << 2 << endl;
+    }
+    else
+    {
+        cout << 1 << endl;
+    }
+}
