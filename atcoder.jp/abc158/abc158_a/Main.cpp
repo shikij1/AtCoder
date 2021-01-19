671e058f667108b4b668a51f04d6c63887fa9e20
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-19 23:39:56 +0900

    [AC] abc158 abc158_a

---
+++ atcoder.jp/abc158/abc158_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    if (s == "AAA" || s == "BBB")
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+}
