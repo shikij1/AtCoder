78fc6e4ff6a39457dbc72325197c3a767de1223c
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-20 01:13:07 +0900

    [AC] abc167 abc167_a

---
+++ atcoder.jp/abc167/abc167_a/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    t.pop_back();
+    (s == t) ? cout << "Yes" << endl : cout << "No" << endl;
+}
