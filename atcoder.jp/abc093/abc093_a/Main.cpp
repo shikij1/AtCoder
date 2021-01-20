70ee494c9a8dd1f555fb8106896602b95507f85d
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-20 10:59:43 +0900

    [AC] abc093 abc093_a

---
+++ atcoder.jp/abc093/abc093_a/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    "abc" == s ? cout << "Yes" << endl : cout << "No" << endl;
+}
