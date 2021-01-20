a74dbd08694a6d2b64a9037a8fb4e08850308ce8
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-20 10:16:23 +0900

    [AC] abc154 abc154_a

---
+++ atcoder.jp/abc154/abc154_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, t, u;
+    int a, b;
+    cin >> s >> t >> a >> b >> u;
+    if (s == u)
+        cout << a - 1 << ' ' << b << endl;
+    else
+        cout << a << ' ' << b - 1 << endl;
+}
