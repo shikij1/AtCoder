4d5318b37ba28bc9581b6b38d4230fd00d6db16b
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-21 16:55:50 +0900

    [AC] abc150 abc150_b

---
+++ atcoder.jp/abc150/abc150_b/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    string S;
+    cin >> N >> S;
+    int ans = 0;
+    for (int i = 0; i < N - 2; i++)
+    {
+        if (S.substr(i, 3) == "ABC")
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
