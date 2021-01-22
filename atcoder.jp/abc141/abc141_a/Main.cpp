520ffe02ca5e935a5cd3901b91e97041a76f0587
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-22 20:07:10 +0900

    [AC] abc141 abc141_a

---
+++ atcoder.jp/abc141/abc141_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    if (s == "Sunny")
+        cout << "Cloudy" << endl;
+    else if (s == "Rainy")
+        cout << "Sunny" << endl;
+    else
+        cout << "Rainy" << endl;
+}
