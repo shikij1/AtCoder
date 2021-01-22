48a2ef2e0e53a5d1771052f4ca7de6dc94852f85
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-23 01:59:47 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int d;
+    cin >> d;
+    if (d == 22)
+        cout << "Christmas Eve Eve Eve" << endl;
+    else if (d == 23)
+        cout << "Christmas Eve Eve" << endl;
+    else if (d == 24)
+        cout << "Christmas Eve" << endl;
+    else
+        cout << "Christmas" << endl;
+}
