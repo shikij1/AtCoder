e879b796568643c2ae7cb558aeaa2ae6e6fe20eb
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-23 19:46:01 +0900

    [AC] abc124 abc124_a

---
+++ atcoder.jp/abc124/abc124_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int ans = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        if (a > b)
+        {
+            ans += a;
+            a--;
+        }
+        else
+        {
+            ans += b;
+            b--;
+        }
+    }
+    cout << ans << endl;
+}
