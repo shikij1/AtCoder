08c785fb36aafa3bd49af0b57d61d4fe625a7ba0
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-24 12:06:22 +0900

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    if (n == 1)
+    {
+        cout << "Hello World" << endl;
+    }
+    else
+    {
+        cout << a + b << endl;
+    }
+}
