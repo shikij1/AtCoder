380a1c86f5994f8480573b78c3d14c7b45bd61d4
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-24 23:57:09 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, k;
+    cin >> a >> b >> c >> d >> e >> k;
+    if (e - a <= k)
+    {
+        cout << "Yay!" << endl;
+    }
+    else
+    {
+        cout << ":(" << endl;
+    }
+}
