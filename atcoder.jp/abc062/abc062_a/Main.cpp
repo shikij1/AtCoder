8076fdaf3303b181db9e83bdd04253b947d39995
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-25 00:49:46 +0900

    [AC] abc062 abc062_a

---
+++ atcoder.jp/abc062/abc062_a/Main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x == 1 || x == 3 || x == 5 || x == 7 || x == 8 || x == 10 || x == 12)
+    {
+        if (y == 1 || y == 3 || y == 5 || y == 7 || y == 8 || y == 10 || y == 12)
+            cout << "Yes" << endl;
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    else if (x == 4 || x == 6 || x == 9 || x == 11)
+    {
+        if (y == 4 || y == 6 || y == 9 || y == 11)
+            cout << "Yes" << endl;
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    else if (x == 2 && y == 2)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
