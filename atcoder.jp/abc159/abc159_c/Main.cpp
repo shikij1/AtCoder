bce44c949384217f6705a7f8c3721633ceb8b4bd
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-28 22:36:51 +0900

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double l;
+    cin >> l;
+    double ans = (l / 3) * (l / 3) * (l / 3);
+    cout << fixed << setprecision(15) << ans << endl;
+}
