0f6e28210f4ce3c03cb4470da1c14819bfa1a3c1
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-28 10:21:10 +0900

    [AC] abc180 abc180_c

---
+++ atcoder.jp/abc180/abc180_c/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    vector<long long> ans;
+    for (long long i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            ans.push_back(i);
+            if (i * i != n)
+                ans.push_back(n / i);
+        }
+    }
+    sort(ans.begin(), ans.end());
+
+    for (auto e : ans)
+    {
+        cout << e << endl;
+    }
+}
