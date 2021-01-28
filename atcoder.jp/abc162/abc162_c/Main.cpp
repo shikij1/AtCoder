a0df2bc03c99636668880d93904ee333c5924fbf
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-28 22:54:07 +0900

    [AC] abc162 abc162_c

---
+++ atcoder.jp/abc162/abc162_c/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k;
+    cin >> k;
+    int ans = 0;
+    for (int a = 1; a <= k; a++)
+    {
+        for (int b = 1; b <= k; b++)
+        {
+            for (int c = 1; c <= k; c++)
+            {
+                ans += __gcd(__gcd(a, b), c);
+            }
+        }
+    }
+    cout << ans << endl;
+}
