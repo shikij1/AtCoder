ebdce738108b0e9ceb76fec6dc7146c1683d5ee4
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-30 02:01:35 +0900

    [AC] abc101 abc101_b

---
+++ atcoder.jp/abc101/abc101_b/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int tmp = n;
+    int sum = 0;
+    while (tmp > 0)
+    {
+        sum += tmp % 10;
+        tmp /= 10;
+    }
+    if (n % sum == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
