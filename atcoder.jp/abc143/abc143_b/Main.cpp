dc05acad210e0f116cd726cd23be75e50ba02736
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-01-30 16:42:24 +0900

    [AC] abc143 abc143_b

---
+++ atcoder.jp/abc143/abc143_b/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> d(n);
+    for (int i = 0; i < n; i++) {
+        cin >> d[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            sum += d[i] * d[j];
+        }
+    }
+    cout << sum << endl;
+}
