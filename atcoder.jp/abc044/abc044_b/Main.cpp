732f347df79ba9db8b18b27e4d2da36e3220552f
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-16 16:57:23 +0900

    [AC] abc044 abc044_b

---
+++ atcoder.jp/abc044/abc044_b/Main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll mod_pow(ll a, ll n, ll mod) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    string w;
+    cin >> w;
+    map<char, int> mp;
+    for (int i = 0; i < (int)w.size(); i++) {
+        mp[w[i]]++;
+    }
+    for (auto x : mp) {
+        if (x.second % 2 != 0) {
+            cout << "No" << '\n';
+            return;
+        }
+    }
+    cout << "Yes" << '\n';
+}
+signed main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
