d6082404eb5717d216e94e88a6f4bd6da707b36e
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-16 17:18:18 +0900

    [AC] abc061 abc061_b

---
+++ atcoder.jp/abc061/abc061_b/Main.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len=(n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len=(n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = { 1, 0, -1, 0 };
+const int dy[4] = { 0, 1, 0, -1 };
+
+ll mod_pow(ll a, ll n, ll mod) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> G(n);
+    int a, b;
+    for (int i =0;i<m;i++){
+        cin >> a >> b;
+        G[a-1].push_back(b-1);
+        G[b-1].push_back(a-1);
+    }
+
+    for (int i =0;i<n;i++){
+       cout << (int)G[i].size() <<'\n';
+    }
+
+}
+
+signed main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
