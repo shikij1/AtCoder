90a41d9e5a8546507483b8d5f48695c4491e9e2a
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-18 14:19:28 +0900

    [AC] abc121 abc121_c

---
+++ atcoder.jp/abc121/abc121_c/Main.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len=(n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len=(n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll mod_pow(ll a, ll n, ll mod) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  ll n, m;
+  cin >> n >> m;
+  vector<pair<ll, ll>> s(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> s[i].first >> s[i].second;
+  }
+  sort(s.begin(), s.end());
+  int i = 0;
+  ll ans = 0;
+  while (m > 0) {
+    ans += s[i].first;
+    m--;
+    s[i].second--;
+    if (s[i].second == 0) {
+      i++;
+    }
+  }
+  cout << ans << '\n';
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
