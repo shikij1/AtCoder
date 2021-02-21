a036226c1326c3d1cff867c9c68daaa215d37339
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-22 07:07:21 +0900

    [AC] panasonic2020 panasonic2020_b

---
+++ atcoder.jp/panasonic2020/panasonic2020_b/Main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len=(n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len=(n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll mod_pow(ll a, ll n, ll mod) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  ll h, w;
+  cin >> h >> w;
+  if(h==1||w==1){
+    cout << 1 << '\n';
+    return;
+  }
+  if (h % 2 == 1 && w % 2 == 1) {
+    cout << (h * w+1) / 2 << '\n';
+  }else{
+    cout << (h * w) / 2 << '\n';
+  }
+
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
