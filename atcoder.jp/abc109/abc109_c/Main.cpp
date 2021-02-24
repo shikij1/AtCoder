8e5e44e83a8a26f54fc6e7ea9fa344671476db85
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-24 13:47:08 +0900

    [AC] abc109 abc109_c

---
+++ atcoder.jp/abc109/abc109_c/Main.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len=(n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len=(n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll mod_pow(ll a, ll n, ll mod) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+int gcd(int x, int y) {
+  if(x % y == 0) {
+    return y;
+  } else {
+    return gcd(y, x % y);
+    //x%y==0でないときはユーグリットの互除法を使って再帰的に関数を呼び出す。
+  }
+}
+
+void solve() {
+  int n, X;
+  cin >> n >> X;
+  int rgcd;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    if(i==0){
+      rgcd = abs(x - X);
+    }
+    rgcd = gcd(rgcd, abs(x - X));
+  }
+  cout << rgcd << '\n';
+
+}
+
+signed main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
