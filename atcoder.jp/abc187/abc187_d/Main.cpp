6f3c237171b3dc4488eb9757bad13c4f2edbd1e1
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-02-28 03:51:18 +0900

    [AC] abc187 abc187_d

---
+++ atcoder.jp/abc187/abc187_d/Main.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n) for (int i = 0, i##_len=(n); i < i##_len; ++i)
+#define reps(i, n) for (int i = 1, i##_len=(n); i <= i##_len; ++i)
+#define rrep(i, n) for (int i = ((int)(n)-1); i >= 0; --i)
+#define rreps(i, n) for (int i = ((int)(n)); i > 0; --i)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const double pi = acos(-1.0);
+const ll mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll mod_pow(ll a, ll n, ll mod) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  ll suma = 0, sumb = 0;
+  vector<tuple<ll, ll, ll>> vec;
+  for (int i = 0; i < n; ++i) {
+    ll a, b;
+    cin >> a >> b;
+    vec.push_back(make_tuple(2 * a + b, a, a + b));
+    suma += a;
+  }
+  sort(vec.rbegin(), vec.rend());
+  for (int i = 0; i < n; ++i) {
+    sumb += get<2>(vec[i]);
+    suma -= get<1>(vec[i]);
+    if (sumb > suma) {
+      cout << i + 1 << '\n';
+      return;
+    }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
