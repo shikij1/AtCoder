e0b16146a9c4418dc9da1331b68ecf485839bd26
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-05-05 02:34:25 +0900

    [AC] abc044 abc044_a

---
+++ atcoder.jp/abc044/abc044_a/Main.cpp
@@ -0,0 +1,27 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int n, k, x, y;
+  cin >> n >> k >> x >> y;
+  if (n <= k) {
+    cout << x * n << '\n';
+  } else {
+    cout << x *  k + y * (n - k) << '\n';
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
