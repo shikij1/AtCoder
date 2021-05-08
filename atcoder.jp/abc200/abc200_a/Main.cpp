000c7fb588b3080641286e512fa014208c25a369
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-05-08 21:03:18 +0900

    [AC] abc200 abc200_a

---
+++ atcoder.jp/abc200/abc200_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int N;
+  cin >> N;
+  for (int i = 1; i < 300; ++i) {
+    if (i<=N && N<=(100 * i) ){
+      cout << i << '\n';
+      return;
+    }
+
+  }
+
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
