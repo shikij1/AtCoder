7dc01e06f016bc9d2edc063a8a747892bb782698
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-05-10 01:42:51 +0900

    [AC] abc022 abc022_b

---
+++ atcoder.jp/abc022/abc022_b/Main.cpp
@@ -0,0 +1,37 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int N;
+  cin >> N;
+  set<int> st;
+  int ans = 0;
+  for (int i = 0; i < N; ++i) {
+    int A;
+    cin >> A;
+    if (st.count(A)){
+      ++ans;
+    }
+    else{
+      st.insert(A);
+    }
+  }
+  cout << ans << '\n';
+  
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
