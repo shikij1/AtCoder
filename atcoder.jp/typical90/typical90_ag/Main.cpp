7de35ded4c8cfec689b05edb2b780de0682d69b3
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-06-06 20:37:17 +0900

    [AC] typical90 typical90_ag

---
+++ atcoder.jp/typical90/typical90_ag/Main.cpp
@@ -0,0 +1,28 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int h, w;
+  cin >> h >> w;
+  if (h == 1 || w == 1) {
+    cout << h * w << '\n';
+  } else {
+    cout << ((h + 1) / 2) * ((w + 1) / 2) << '\n';
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
