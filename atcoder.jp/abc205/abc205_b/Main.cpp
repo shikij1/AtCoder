9c5b3afe32adba6cd5d301a8f8c65f557cfe5cb5
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-06-13 21:05:25 +0900

    [AC] abc205 abc205_b

---
+++ atcoder.jp/abc205/abc205_b/Main.cpp
@@ -0,0 +1,36 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != (i+1)){
+      cout << "No" << '\n';
+      return;
+    }
+  }
+  cout << "Yes" << '\n';
+  
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
