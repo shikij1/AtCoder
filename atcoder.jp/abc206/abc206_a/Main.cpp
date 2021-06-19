a8e17d5fed2072211da6cd297019b9b9d4824a99
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-06-19 21:03:27 +0900

    [AC] abc206 abc206_a

---
+++ atcoder.jp/abc206/abc206_a/Main.cpp
@@ -0,0 +1,34 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  int n;
+  cin >> n;
+  int res = n * 1.08;
+  if (res<206){
+    cout << "Yay!" << '\n';
+  }
+  else if (res == 206){
+    cout << "so-so" << '\n';
+  }
+  else{
+    cout << ":(" << '\n';
+    
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
