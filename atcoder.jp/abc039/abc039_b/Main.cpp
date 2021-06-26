5a45bed451f09c3dedd2cb898f1d7696bac4f7bf
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-06-26 20:40:03 +0900

    [AC] abc039 abc039_b

---
+++ atcoder.jp/abc039/abc039_b/Main.cpp
@@ -0,0 +1,28 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  long long x;
+  cin >> x;
+  for (long long i = 0; i < 1e3; ++i) {
+    if (i * i * i * i == x) {
+      cout << i << '\n';
+      return;
+    }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
