ee5ace0d7b1555dc6e6137928e9c1c1f6facc6be
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-06-26 21:01:47 +0900

    [AC] abc207 abc207_a

---
+++ atcoder.jp/abc207/abc207_a/Main.cpp
@@ -0,0 +1,27 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  vector<int> v(3);
+  for (int i = 0; i < 3; ++i) {
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end());
+  cout << v[1] + v[2] << '\n';
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
