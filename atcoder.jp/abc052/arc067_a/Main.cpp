489f5060aa2351ea9fda81945cb8f0d6fa01cea6
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-07-10 18:55:34 +0900

    [AC] abc052 arc067_a

---
+++ atcoder.jp/abc052/arc067_a/Main.cpp
@@ -0,0 +1,57 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+/*  prime_factor(n)
+    入力：整数 n
+    出力：nの素因数分解
+    計算量：O(√n)前後
+*/
+template<typename T>
+vector<pair<T, T>> prime_factor(T n) {
+  vector<pair<T, T>> ret;
+  for (T i = 2; i * i <= n; i++) {
+    if (n % i != 0) continue;
+    T tmp = 0;
+    while (n % i == 0) {
+      tmp++;
+      n /= i;
+    }
+    ret.push_back(make_pair(i, tmp));
+  }
+  if (n != 1) ret.push_back(make_pair(n, 1));
+  return ret;
+}
+
+void solve() {
+  long long N;
+  cin >> N;
+  vector<long long> a(1e4);
+  for (long long i = 2; i <= N; ++i) {
+    vector<pair<long long, long long>> v = prime_factor(i);
+    for (auto e : v) {
+      a[e.first] += e.second;
+    }
+  }
+  mint ans = 1;
+  for (auto e : a) {
+    if (e > 0) {
+      ans *= (e + 1);
+    }
+  }
+  cout << ans.val() << '\n';
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
