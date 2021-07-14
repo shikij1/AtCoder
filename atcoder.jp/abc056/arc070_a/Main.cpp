b32739b30ca7979106e92645becd31d506b0d52d
Zel3o <56910224+shikij1@users.noreply.github.com>
2021-07-15 01:22:58 +0900

    [AC] abc056 arc070_a

---
+++ atcoder.jp/abc056/arc070_a/Main.cpp
@@ -0,0 +1,31 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace atcoder;
+using namespace std;
+using mint = modint1000000007;
+// using mint = modint998244353;
+
+long long gcd(long long x, long long y) { return (x % y) ? gcd(y, x % y) : y; }
+long long lcm(long long x, long long y) { return x / gcd(x, y) * y; }
+
+void solve() {
+  long long x;
+  cin >> x;
+  long long sum = 0;
+  for (int i = 1; i < 1e8; ++i) {
+    sum += i;
+    if (sum >= x) {
+      cout << i << '\n';
+      return;
+    }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+  solve();
+  return 0;
+}
